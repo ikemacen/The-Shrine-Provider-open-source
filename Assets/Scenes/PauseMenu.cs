d66e89c355cdeb6c64198eaa6b8139763b3a3f3a
Justin Chen <jchen6648@gmail.com>
2024-07-24 18:19:49 -0500

    Added Camera Scenes and Menu Scenes
    Made 4 different Camera Scenes and added UI to the game

---
+++ Assets/Scenes/PauseMenu.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Pause();
+            if (Time.timeScale == 0)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void LvlLoad(string lv)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(lv);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
+
