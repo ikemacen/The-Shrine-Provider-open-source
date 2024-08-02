32f3f22fc30771fb22fc14cab11e6b56aa422e61
ikemacen <michaeljbrunt@gmail.com>
2024-08-02 12:17:10 -0400

    Revert "fixing changes"
    This reverts commit 6d0cdf03077e4a74500a8d4b7ea7039b01813f05.

--- Assets/Scripts/Player control.cs
+++ Assets/Scripts/Player control.cs
@@ -11,46 +11,68 @@ public class Playercontrol : MonoBehaviour
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _speed = 5;
     [SerializeField] private float _turnspeed = 360;
-    private Vector3 _Input;
-    public Vector3 Initalrotation = new Vector3(-90, 0, 0);
+    private Vector2 _input;
+    private Vector3 _movementDirection;
+    public Vector3 initalrotation = new Vector3(-90,0,0);
 
     private void Start()
     {
-        transform.rotation = Quaternion.Euler(Initalrotation);
+        //transform.rotation = Quaternion.Euler(initalrotation);
+        transform.rotation = Quaternion.identity;
     }
     void Update()
     {
-        GatherInput();
-        look();
+        _input = GatherInput();
+        _movementDirection = CalculateDirection(_input);
+        // Normalize here to prevent faster diagonal movement
+        _movementDirection.Normalize();
+        print(_movementDirection);
+        // Only write to rotation if movement is not zero
+        // Otherwise, don't rotate
+        if (_movementDirection != Vector3.zero)
+        {
+            transform.rotation = look(_movementDirection);
+        }
     }
     void FixedUpdate()
     {
         Move();
     }
-  void look ()
+    Quaternion look(Vector3 MovementDirection)
     {
-        if (_Input != Vector3.zero)
-        {
-            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
-            var skewedInput = matrix.MultiplyPoint3x4(_Input);
+        //var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+        //var skewedInput = matrix.MultiplyPoint3x4(_Input);
 
-            var relative = (transform.position + _Input) - transform.position;
-            var rot = Quaternion.LookRotation(relative, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnspeed * Time.deltaTime);
-        }
+        var relative = (transform.position + MovementDirection) - transform.position;
+        var rot = Quaternion.LookRotation(relative, Vector3.up);
+        return Quaternion.RotateTowards(transform.rotation,rot, _turnspeed * Time.deltaTime);
     }
 
-    void GatherInput()
+    // Read input and translate into an input vector
+    Vector2 GatherInput()
     {
         float xDirection = Input.GetAxisRaw("Horizontal");
         float yDirection = Input.GetAxisRaw("Vertical");
 
-        _Input = new Vector3(xDirection, 0, yDirection);
-        _Input.Normalize();
+        return new Vector2(xDirection, yDirection);
     }
+
+    // Turn the input vector into the correct 3d directional vectors
+    Vector3 CalculateDirection(Vector2 input) {
+        // These are the correct movement values according to each input axis
+        Vector3 adjustedXVector = new Vector3(-1, 0, 1);
+        Vector3 adjustedZVector = new Vector3(1, 0, 1);
+        // We calculate the correct movement direction
+        adjustedXVector *= -_input.x;
+        adjustedZVector *= _input.y;
+        // Add both directions together to get the finalized movement direction
+        return adjustedZVector + adjustedXVector;
+    }
+
+    // Execute the movement to our player rigidbody
     void Move()
     {
-        _rb.MovePosition(transform.position + (transform.forward * _Input.magnitude) * _speed * Time.deltaTime);
+        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
     }
 
 }
