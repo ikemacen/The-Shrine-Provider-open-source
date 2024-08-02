e7855a225c8b267da810cbedb6be55fb8f7fcb46
Michael <l4wmich43l@gmail.com>
2024-08-02 14:15:42 -0700

    Adding a basic Tool Class

---
+++ Assets/Scripts/Tool Scripts/SeedPouch.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedPouch : ToolClass 
+{
+    new public void InteractAction()
+    {
+        print("Look at me go, planting a seed!");
+    }
+}
--- Assets/Scripts/Tool Scripts/ToolClass.cs
+++ Assets/Scripts/Tool Scripts/ToolClass.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class ToolClass : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void InteractAction() {
+        print("If you are seeing this, you are stinky.");
     }
 }
