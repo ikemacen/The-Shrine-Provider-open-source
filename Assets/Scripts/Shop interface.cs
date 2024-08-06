0a283af0257031ac2e0dd012f8b0a73874b3c2af
ikemacen <michaeljbrunt@gmail.com>
2024-08-05 23:52:33 -0400

    Created shop UI
    Created the shop UI and system that would allow a more easier time to add and store items into the shop, Also have a player script to store vaules on the player and have an interface that can interact with other scripts to allow access between both the stored vaules and other scripts. do not have a button interaction yet since we do not have items to activate so it remains in pending. the shop desgin is WIP

---
+++ Assets/Scripts/Shop interface.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Shopinterface 
+{
+    void Boughtitem(Items.Itemtype itemtype); //interface used for buying items in the shop
+}
